0403c861f70bbe61440b3c5730b6c2726477191a
Nicole D <nnardella@gmail.com>
2018-09-21 00:44:51 +0000

    automatically backed up by learn

--- simple_looping.rb
+++ simple_looping.rb
@@ -2,14 +2,15 @@
 
 def loop_iterator(number_of_times)
 number_of_times = 0
+
 loop do
   number_of_times += 1
   puts "Welcome to Flatiron School's Web Development Course!"
 
   if number_of_times >=7
     break
-end
-end
+  end
+ end
 end
 
 def times_iterator(number_of_times)
